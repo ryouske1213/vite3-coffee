b6f31e1d895d0e625715f847e0b331cf84939e02
ryouske1213 <ryouske@gamil.com>
2023-11-18 22:37:33 +0800

    fix: 修改shopping add sub del 錯誤

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -15,34 +15,33 @@ onMounted(() => {
     JSON.parse(localStorage.getItem("currenItem", "currenItem")) ?? [];
 });
 
-const add = () => {
-  coffeeData.value.forEach((item) => {
-    item.count++;
-    console.log(item.count);
-  });
-};
-
-const sub = () => {
-  coffeeData.value.forEach((item, index) => {
-    item.count--;
-    if (item.count <= 0) {
-      item.count = 1;
-      coffeeData.value.splice(index, 1);
-    }
-  });
+const add = (item) => {
+  item.count++;
   window.localStorage.setItem("currenItem", JSON.stringify(coffeeData.value));
 };
 
-const del = () => {
-  coffeeData.value.forEach((item, index) => {
-    coffeeData.value.splice(index, 1);
-  });
+const sub = (item, index) => {
+  item.count--;
+  if (item.count <= 0) {
+    item.count = 1;
+  }
+  coffeeData.value.splice(index, 1);
+
+  window.localStorage.setItem("currenItem", JSON.stringify(coffeeData.value));
+};
+
+const del = (index) => {
+  coffeeData.value.splice(index, 1);
   window.localStorage.setItem("currenItem", JSON.stringify(coffeeData.value));
 };
 </script>
 
 <template>
-  <div v-for="item in coffeeData" :key="item.id" class="w-full justify-center">
+  <div
+    v-for="(item, index) in coffeeData"
+    :key="item.id"
+    class="w-full justify-center"
+  >
     <div class="md:flex w-full my-2">
       <div class="md:flex">
         <div
@@ -64,20 +63,20 @@ const del = () => {
           <div class="md:w-40 flex justify-center">
             <p class="flex justify-center items-center md:w-full pr-4">數量</p>
             <div class="flex items-center px-2 cursor-pointer">
-              <div @click="sub()">-</div>
+              <div @click="sub(item, index)">-</div>
             </div>
             <div class="flex px-4">
               <p class="flex justify-center">{{ item.count }}</p>
             </div>
             <div class="flex items-center px-2 cursor-pointer">
-              <div @click="add()">+</div>
+              <div @click="add(item)">+</div>
             </div>
           </div>
           <div class="flex justify-center items-center my-3 md:pl-10">
             <div
               class="bg-slate-800 w-20 h-10 text-white flex items-center justify-center"
             >
-              <p @click="del()">刪除</p>
+              <p @click="del(index)">刪除</p>
             </div>
           </div>
         </div>
